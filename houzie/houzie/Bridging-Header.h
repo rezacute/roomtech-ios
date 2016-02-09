8cde064637371a5b334e16366878d5477167071b
Abdurrosyid Broto Handoyo <arrosyidbh@gmail.com>
2016-02-10 00:16:16 +0900

    chat controller

--- houzie/houzie/Bridging-Header.h
+++ houzie/houzie/Bridging-Header.h
@@ -11,5 +11,6 @@
 
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <JSQMessagesViewController/JSQMessages.h> 
 
 #endif /* Bridging_Header_h */
---
+++ houzie/houzie/ChatController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatController.swift
+//  houzie
+//
+//  Created by Abdurrosyid Handoyo on 2/10/16.
+//  Copyright © 2016 roomtech. All rights reserved.
+//
+
+import UIKit
+
+class ChatController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
