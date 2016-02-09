45272b0d1115f67aeca1d7234dcc482ce75a38b7
Abdurrosyid Broto Handoyo <arrosyidbh@gmail.com>
2016-02-09 18:14:01 +0900

    facebook sign-in and google sign-in

---
+++ houzie/houzie/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  houzie
+//
+//  Created by Abdurrosyid Broto Handoyo on 2016/02/09.
+//  Copyright © 2016年 roomtech. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
--- houzie/houzie/ViewController.swift
+++ houzie/houzie/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in
+        }
+        else
+        {
+            let loginButton = FBSDKLoginButton()
+            self.view.addSubview(loginButton)
+            loginButton.center = self.view.center
+            loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+            loginButton.delegate = self
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+        if (error != nil) {
+            print(error.description)
+        } else if result.isCancelled {
+            print(result.description)
+        } else {
+            
+        }
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("Logged out")
+    }
 
 }
 
