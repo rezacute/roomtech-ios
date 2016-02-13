021d1b8f9457055e5d55348c67725447c880ad1d
Abdurrosyid Broto Handoyo <arrosyidbh@gmail.com>
2016-02-13 15:48:37 +0900

    chat ui

--- houzie/houzie/ChatController.swift
+++ houzie/houzie/ChatController.swift
@@ -13,6 +13,12 @@ class ChatController: JSQMessagesViewController {
     // MARK: Variable declaration
     
     let messageData: NSMutableArray = []
+    let incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImageWithColor(UIColor.lightGrayColor())
+    let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImageWithColor(UIColor(red: 10/255, green: 180/255, blue: 230/255, alpha: 1.0))
+    let senderAvatar = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "avatar.png"), diameter: 30)
+    let botId = "90283-412412-41"
+    let botDisplayName = "Roomtech Bot"
+    let botAvatar = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "bot.png"), diameter: 30)
     
     // MARK: View Lifecyle
     
@@ -22,15 +28,44 @@ class ChatController: JSQMessagesViewController {
         self.title = "Houzie"
         
         self.senderId = "053496-4509-289"
-        self.senderDisplayName = "Test"
+        self.senderDisplayName = "Handoyo"
         self.showLoadEarlierMessagesHeader = true
-
+        
+        initMessageData()
+    }
+    
+    // MARK: Initialization
+    
+    func initMessageData() {
+        self.messageData.addObject(JSQMessage(
+            senderId: self.senderId,
+            senderDisplayName: self.senderDisplayName,
+            date: NSDate.distantPast(),
+            text: "!smarthome get temperature"
+            )
+        )
+        self.messageData.addObject(JSQMessage(
+            senderId: self.botId,
+            senderDisplayName: self.botDisplayName,
+            date: NSDate.distantPast(),
+            text: "Current Temperature is 28.59°C"
+            )
+        )
     }
     
     // MARK: JSQMessagesViewController method overrides
     
     override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
         JSQSystemSoundPlayer.jsq_playMessageSentSound()
+        
+        self.messageData.addObject(JSQMessage(
+            senderId: senderId,
+            senderDisplayName: senderDisplayName,
+            date: date,
+            text: text
+            )
+        )
+        
         self.finishSendingMessageAnimated(true)
     }
     
@@ -45,27 +80,46 @@ class ChatController: JSQMessagesViewController {
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!, didDeleteMessageAtIndexPath indexPath: NSIndexPath!) {
-        
+        self.messageData.removeObjectAtIndex(indexPath.row)
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
-        return nil
+        let message = self.messageData[indexPath.row]
+        switch(message.senderId()) {
+            case self.senderId:
+                return self.outgoingBubble
+            default:
+                return self.incomingBubble
+        }
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
-        return nil
-    }
-    
-    override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForCellTopLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString! {
-        return nil
+        let message = self.messageData[indexPath.row]
+        switch(message.senderId()) {
+            case self.senderId:
+                return self.senderAvatar
+            default:
+                return self.botAvatar
+        }
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString! {
-        return nil
+        let message = self.messageData[indexPath.row]
+        return NSAttributedString(string: message.senderDisplayName())
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForCellTopLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString! {
+        let message = self.messageData[indexPath.row]
+        
+        return NSAttributedString(string: message.senderDisplayName())
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForCellBottomLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString! {
-        return nil
+        let message = self.messageData[indexPath.row]
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        
+        return NSAttributedString(string: dateFormatter.stringFromDate(message.date!!))
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
